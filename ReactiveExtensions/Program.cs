7b7b43c46b1115c1b93f22bc3f4a235f153f13e7
Stepan Kurdylo <stepankurdylo84@gmail.com>
2019-02-19 13:42:50 +0200

    Add project files.

---
+++ ReactiveExtensions/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ReactiveExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reactive.Linq;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            KeyPresses().ToObservable().Buffer(3).Subscribe(keyPress =>
+            {
+                keyPress.Select(key => key.Key).ToList().ForEach(keyChar => Console.WriteLine($"\"{keyChar}\""));
+            });
+        }
+
+        // Infinity Loop
+        private static IEnumerable<ConsoleKeyInfo> KeyPresses()
+        {
+            for (;;)
+            {
+                var currentKey = Console.ReadKey(true);
+                if (currentKey.Key == ConsoleKey.Enter)
+                {
+                    yield break;
+                }
+                else
+                {
+                    yield return currentKey;
+                }
+            }
+        }
+    }
+}
